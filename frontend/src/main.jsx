ca27e89d0a5db9b073f023385a9b2d55b10bff77
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2023-07-19 09:43:13 +0200

    préparation du front

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { LudoProvider } from "./context/LudoContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <LudoProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </LudoProvider>
   </BrowserRouter>
 );
